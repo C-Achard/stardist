85d112999b7862c5e13305b1f0fd6345e77401f3
C-Achard <cyril.achard@epfl.ch>
2023-07-21 14:40:46 +0200

    Instance benchmark

--- examples/3D/benchmark_instance.py
+++ examples/3D/benchmark_instance.py
@@ -1,9 +1,11 @@
-from pathlib import Path
-from stardist.matching import matching_dataset
+from tqdm.notebook import tqdm
+import matplotlib.pyplot as plt
+from tqdm.notebook import tqdm
+import matplotlib.pyplot as plt
 
-def plot_performance(taus, stats):
+def plot_performance(taus, stats, name):
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 5))
-
+    fig.suptitle(name)
     for m in ('precision', 'recall', 'accuracy', 'f1', 'mean_true_score', 'mean_matched_score', 'panoptic_quality'):
         ax1.plot(taus, [s._asdict()[m] for s in stats], '.-', lw=2, label=m)
     ax1.set_xlabel(r'IoU threshold $\tau$')
@@ -22,6 +24,19 @@ def plot_performance(taus, stats):
 
 if __name__=="__main__":
     path_images = Path.home() / "Desktop/Code/CELLSEG_BENCHMARK"
-    gt_path = path_images / "TPH2_DATA/visual_iso/labels/visual_labels.tif"
+    gt_path = path_images / "TPH2_mesospim/test_data/labels/isotropic_visual.tif"
+    results = path_images / "RESULTS/full data/instance"
+    predictions = [imread(str(p)) for p in sorted(results.glob("*.tif"))]
+    pred_names = [p.name for p in sorted(results.glob("*.tif"))]
 
+    taus = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+
+    Y_val = imread(str(gt_path))
+    for i, p in enumerate(predictions):
+        Y_val_pred = p
+        print(f"Validating on {pred_names[i]}")
+        stats = [matching_dataset(Y_val, Y_val_pred, thresh=t, show_progress=False) for t in tqdm(taus)]
+        plot_performance(taus, stats, name=pred_names[i])
+        plt.show()
+        print("*"*20)
 
